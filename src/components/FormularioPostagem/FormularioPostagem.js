2166fd46061a354f2cf8919d831803e0311e6b52
Amanda <amandaoliveirapolari@gmail.com>
2023-05-29 14:57:59 -0300

    Relembrando inputs controlados

--- src/components/FormularioPostagem/FormularioPostagem.js
+++ src/components/FormularioPostagem/FormularioPostagem.js
@@ -1,6 +1,14 @@
 import { FormContainer, Form, Input, StyledLabel } from "./FormularioPostagem.styled";
 
 export const FormularioCadastro = () => {
+  // INPUTS CONTROLADOS | 3 PASSOS:
+  // 1. CRIAR OS ESTADOS PARA CADA INPUT
+  // 2. CRIAR AS FUNÇÕES QUE IRÃO MUDAR OS ESTADOS A PARTIR DE UM ONCLICK
+  // 3. CRIAR UM ONCHANGE PARA CADA INPUT E PASSAR PARA DENTRO DELAS A FUNÇÃO QUE FAZ AS MUDANÇAS SETANDO O ESTADO
+  // 4. O VALUE DE CADA INPUT SERA O VALOR DO ESTADO
+  // ------------------------------------------
+
+  
   return (
     <FormContainer>
       <h1>Insira sua postagem abaixo: </h1>
